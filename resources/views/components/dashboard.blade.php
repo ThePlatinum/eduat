df6e40b9b1cdbfbc95946bd837b128fdd4fc75d2
Platinum <platinumemirate@gmail.com>
2022-05-05 16:57:38 +0100

    role dashboard

--- resources/views/components/dashboard.blade.php
+++ resources/views/components/dashboard.blade.php
@@ -6,23 +6,21 @@
     <h5>Dashboard</h5>
   </div>
 
-  <!-- @role('Admin')
-        @include('dashboard.admin')
-    @endrole
-
-    @role('Accountant')
-        @include('dashboard.accountant')
-    @endrole
-
-    @role('Teacher')
-        @include('dashboard.teacher')
-    @endrole
-
-    @role('Student')
-        @include('dashboard.student')
-    @endrole -->
-
+  @role('Admin')
   @include('dashboard.admin')
+  @endrole
+
+  @role('Accountant')
+  @include('dashboard.accountant')
+  @endrole
+
+  @role('Teacher')
+  @include('dashboard.teacher')
+  @endrole
+
+  @role('Student')
+  @include('dashboard.student')
+  @endrole
 
 </div>
 @endsection
