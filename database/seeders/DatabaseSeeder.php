c0c35640880ae20edac857e07b4f6179d7a8be7f
Platinum <platinumemirate@gmail.com>
2022-05-05 16:10:58 +0100

    roles seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,32 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-    }
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    // \App\Models\User::factory(10)->create();
+
+    $this->call(RolesAndPermissionsSeeder::class);
+
+    $user = User::create([
+      'firstname' => 'Super',
+      'lastname'  => 'Eduat',
+      'othername' => 'Admin',
+      'email'     => 'admin@eduat.com',
+      'password'  => Hash::make('12345678'),
+      'created_at'=> now(),
+    ]);
+
+    $user->assignRole('Admin');
+  }
 }
