00da19e41d7fe97812bd47c4ba0146e998cd1077
Platinum <platinumemirate@gmail.com>
2022-06-08 22:45:12 +0100

    import bootstrap

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,7 +11,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <!-- Styles -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link href="{{asset('bootstrap5.1.3/bootstrap.min.css')}}" rel="stylesheet">
 
   <style>
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
