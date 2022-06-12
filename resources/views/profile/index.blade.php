1ea3801fa66355a3b8e03b8afd43806310d98d9e
Platinum <platinumemirate@gmail.com>
2022-06-12 22:49:52 +0100

    format dob

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -17,7 +17,7 @@
     </div>
     <div class="col-md-6 profile p-3">
       <p>Date of Birth</p>
-      {{$user->dob ?? 'Not Provided'}}
+      {{date_format($user->dob, 'd, D M, Y') ?? 'Not Provided'}}
 
       <p>Phone</p>
       {{$user->phone ?? 'Not Provided'}}
