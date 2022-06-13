98a125833dbaaf73e1a9f336610df375358119df
Platinum <platinumemirate@gmail.com>
2022-06-13 08:06:48 +0100

    fix: seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\ClassTeacher;
 use App\Models\Klass;
 use App\Models\Settings;
+use App\Models\StudentClasses;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -64,18 +65,23 @@ class DatabaseSeeder extends Seeder
     $user->assignRole('Teacher');
 
     // Create Student
-    $user = User::create([
+    $student = User::create([
       'firstname' => 'Eduat',
       'lastname'  => 'Student',
-      'othername' => 'User',
+      'othername' => 'Test User',
       'email'     => 'student@eduat.com',
       'phone'     => '+2347098765432',
-      'klass_id'  => Klass::all()->random()->id +1,
-      'password'  => Hash::make('12345678'),
-      'created_at'=> now(),
+      'klass_id'  => Klass::where('id','>',1)->get()->random()->id,
+      'gender' => 'Male',
+      'dob' => '2015-03-03',
+      'address' => 'No. 12, Oloyede Villa, Ogbomoso Str. Lagos State',
+      'password'  => Hash::make('12345678')
     ]);
-
-    $user->assignRole('Student');
+    StudentClasses::create([
+      'student_id' => $student->id,
+      'class_id' => $student->klass_id,
+    ]);
+    $student->assignRole('Student');
 
     // Assign Class Teachers
     $teachers = User::role('Teacher')->get();
