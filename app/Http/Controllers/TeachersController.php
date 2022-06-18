4905e70ea2da43df975ee7b5dfa4687188e6dee2
Platinum <platinumemirate@gmail.com>
2022-06-18 09:01:41 +0100

    fix: employ

--- app/Http/Controllers/TeachersController.php
+++ app/Http/Controllers/TeachersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
 
 class TeachersController extends Controller
 {
@@ -14,7 +15,7 @@ class TeachersController extends Controller
     $teachers = User::whereHas("roles", function($q) {
       $q->where("name", "Teacher");
     })->get();
-    return view('components.teachers', compact("teachers"));
+    return view('teachers.index', compact("teachers"));
   }
 
   public function newteacher()
@@ -24,10 +25,24 @@ class TeachersController extends Controller
 
   public function employ(Request $request){
 
+    $validator = validator($request->all(), [
+      'firstname' => 'required|string|max:255|min:3',
+      'lastname' => 'required|string|max:255',
+      'othername' => 'nullable|string|max:255',
+      'gender' => ['required',Rule::in(['Male', 'Female'])],
+      'email' => 'required|string|email|max:255|unique:users',
+      'phone' => 'required|string|max:255'
+    ]);
+
+    if ($validator->fails()) {
+      return back()->withErrors($validator)->withInput();
+    }
+
     $user = User::create([
       'firstname' => $request->firstname,
       'lastname' => $request->lastname,
       'othername' => $request->othername,
+      'gender' => $request->gender,
       'email' => $request->email,
       'phone' => $request->phone,
       'password' => Hash::make($request->firstname),
