04d32608e3103a1505c7744e17125e5bef82de59
Platinum <platinumemirate@gmail.com>
2022-08-25 20:40:12 +0100

    forceScheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,9 +27,9 @@ class AppServiceProvider extends ServiceProvider
   public function boot()
   {
     //
-    // if (config('app.env') === 'production') {
-    //   URL::forceScheme('https');
-    // }
+    if (config('app.env') === 'production') {
+      URL::forceScheme('https');
+    }
     Schema::defaultStringLength(191);
 
     Paginator::useBootstrap();
